abc96dcb0fc99119f211562d89296542568c20f0
Lance Byrd <meenyleeny@lances-mbp.lan>
2022-01-11 10:57:25 -0500

    setup to begin secureStore calls

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ import App from './App';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Remote debugger']);
