32bb3d05a02cfe8978b6ccc14f7b594d9e50ae4a
Lance Byrd <meenyleeny@lances-mbp.lan>
2022-01-11 10:57:25 -0500

    setup to begin secureStore calls

--- App.tsx
+++ App.tsx
@@ -1,26 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, NativeModules } from 'react-native';
-import { useTranslation } from 'react-i18next'
-import './localization'
+import { StyleSheet, Text, TextInput, View, Button, NativeModules } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import './localization';
+import { React, useState } from 'react';
+import { SecureStore } from 'expo-secure-store';
+
 
 const { CalendarModule, PrismModule } = NativeModules;
 
 export default function App() {
   const { t, i18n } = useTranslation()
-  
-  const onPress = () => {
-    console.log('DID: ' + PrismModule.createDID("passphrase"));
-  };
+
+  const [passphrase, setPassPhrase] = useState('Enter passphrase');
+
+  const [did, setDID] = useState('Your Generated DID');
 
   return (
     <View style={styles.container}>
       <Text>{t('Home.Title')}</Text>
-      <StatusBar style="auto" />
-      <Button
-        title={t('Home.GenerateDIDButton')}
-        color="#841584"
-        onPress={onPress}
+      {}
+      <TextInput
+        style={styles.textInput}
+        clearTextOnFocus={true}
+        multiline={false}
+        onChangeText={text => setPassPhrase(text)}
+        value={passphrase}
       />
+      {}
+      <Button
+        title="Generate DID"
+        onPress={() => {
+          setDID(generateDID(passphrase));
+        }}
+      />
+      {}
+      <TextInput
+        style={styles.textInput}
+        clearTextOnFocus
+        multiline={false}
+        editable={false}
+        value={did}
+      />
+      {}
+       <Button
+          title="Save Passphrase and DID"
+          onPress={() => {
+            savePassDID(passphrase, did);
+           }}
+        />
     </View>
   );
 }
@@ -32,4 +59,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+      height: 35,
+      borderColor: 'gray',
+      borderWidth: 0.5,
+      padding: 4,
+    },
 });
+
+function generateDID(passphrase) {
+  let did = PrismModule.createDID(passphrase)
+  let output = 'From passphrase: '+passphrase+'\ngenerated DID:\n'+did
+  console.log(output);
+  return did
+}
+
+async function savePassDID(key,did) {
+  //await SecureStore.setItemAsync(key, did);
+  console.log('Saved key: ' + key + ' and DID: ' + did);
+}
+
+async function retrieveDIDByKey(key) {
+    let did = await SecureStore.getItemAsync(key);
+    console.log("Here's your DID \n" + did + "\n generated from key " + key);
+}
