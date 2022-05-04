d9bd47cf8304fda669b83c6b791e633a911ff447
Lance <2byrds@gmail.com>
2022-05-04 13:07:03 -0400

    54 import export revoke and verify credential (#55)
    * displaying qr codes for did and cred
    
    * started qr code scanning
    
    * some small fixes/upgrades
    
    * testing (#56)
    
    Co-authored-by: 2byrds <lance.byrd@rootsid.com>
    
    * toggle qrcode and text view
    
    * some more relationship work
    
    * users are a little richer.  skipping credential verification
    
    Co-authored-by: 2byrds <lance.byrd@rootsid.com>

---
+++ src/relationships/index.ts
@@ -0,0 +1,95 @@
+import * as models from '../models'
+import { logger } from '../logging'
+import * as store from '../store'
+
+import apLogo from '../assets/ATALAPRISM.png';
+import butchLogo from '../assets/butch.png'
+import darrellLogo from '../assets/darrell.png'
+import estebanLogo from '../assets/esteban.png'
+import iogLogo from '../assets/iog.png'
+import lanceLogo from '../assets/lance.png'
+import perLogo from '../assets/smallBWPerson.png';
+import rodoLogo from '../assets/rodo.png'
+import rwLogo from '../assets/LogoOnly1024.png';
+import tonyLogo from '../assets/tony.png'
+
+export const rootsLogo = rwLogo;
+export const personLogo = perLogo;
+export const prismLogo = apLogo;
+
+export const ROOTS_BOT = "did:prism:rootsbot1";
+export const PRISM_BOT = "did:prism:prismbot1";
+export const LIBRARY_BOT = "did:prism:librarybot1";
+const IOG_TECH = "did:prism:iogtech1";
+const ROOTSID = "did:prism:rootsid";
+const LANCE = "did:prism:lance";
+const TONY = "did:prism:tony";
+const DARRELL = "did:prism:darrell";
+const BUTCH = "did:prism:butch";
+const ESTEBAN = "did:prism:esteban";
+const RODO = "did:prism:rodolfo";
+
+export const allRelsRegex = new RegExp(models.getStorageKey("",models.MODEL_TYPE_REL)+'*')
+
+//TODO unify aliases and storageKeys?
+export async function createRelItem(alias: string, name: string, pic: string) {
+    try {
+        logger("create rel item",alias,name,pic);
+        if(getRelItem(alias)) {
+            logger("rels - rel already exists",alias)
+            return true;
+        } else {
+            logger("rels - rel did not exist",alias)
+            const relItem = models.createRel(alias, name, pic)
+            const relItemJson = JSON.stringify(relItem)
+            logger("rels - generated rel",relItemJson)
+            const result = await store.saveItem(models.getStorageKey(alias, models.MODEL_TYPE_REL), relItemJson)
+            logger("rels - created rel",alias,"?",result)
+            return result;
+        }
+    } catch(error) {
+        console.error("Failed to create rel",alias,error,error.stack)
+        return false
+    }
+}
+
+export function getRelationships() {
+    logger("rels - getting rel items")
+    const relItemJsonArray = store.getItems(allRelsRegex)
+    logger("rels - got rel items",String(relItemJsonArray))
+    const rels = relItemJsonArray.map(relItemJson => JSON.parse(relItemJson))
+    return rels;
+}
+
+export function getRelItem(relId) {
+    logger("rels - Getting rel",relId)
+    if(relId) {
+        const relItemJson = store.getItem(models.getStorageKey(relId,models.MODEL_TYPE_REL));
+        logger("rels - Got rel json",relItemJson)
+        if(relItemJson) {
+            const relItem = JSON.parse(relItemJson)
+            logger("rels - rel w/keys",Object.keys(relItem))
+            return relItem
+        } else {
+            logger("rels - rel not found",relId)
+            return relItemJson
+        }
+    } else {
+        logger("rels - can't get rel for undefined relId",relId)
+    }
+}
+
+export async function initDemoRels() {
+    logger("rels - init demo rels")
+    await createRelItem(ROOTS_BOT,"RootsWallet",rootsLogo)
+    await createRelItem(PRISM_BOT,"Atala Prism",prismLogo)
+    await createRelItem(LIBRARY_BOT,"Library",personLogo)
+    await createRelItem(IOG_TECH, "IOG Tech Community",iogLogo);
+    await createRelItem(ROOTSID, "RootsID",rootsLogo);
+    await createRelItem(LANCE, "MeGrimLance",lanceLogo);
+    await createRelItem(TONY,"Tony.Rose",tonyLogo)
+    await createRelItem(DARRELL,"Darrell O'Donnell",darrellLogo)
+    await createRelItem(BUTCH,"Butch Clark",butchLogo)
+    await createRelItem(ESTEBAN,"Esteban Garcia",estebanLogo)
+    await createRelItem(RODO,"Rodolfo Miranda",rodoLogo)
+}
--- src/screens/ShowQRCodeScreen.js
+++ src/screens/ShowQRCodeScreen.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import {
   Animated,
   View,
@@ -10,15 +11,47 @@ import { useTheme } from '@react-navigation/native';
 import { useCardAnimation } from '@react-navigation/stack';
 
 import {logger} from '../logging';
+import { ToggleButton } from 'react-native-paper';
 import QRCode from 'react-native-qrcode-svg';
 import { prismLogo } from '../roots'
 import styles from "../styles/styles";
 
 export default function ShowQRCodeScreen({ route, navigation }) {
-  logger("get qr code for", route.params.qrdata)
+    logger("get qr code for", route.params.qrdata)
+    const qrView = 1
+    const textView = 2
+    const [viewSelection, setViewSelection] = useState(qrView)
+    const [viewIcon, setViewIcon] = useState("toggle-switch")
+    const [viewOut, setViewOut] = useState(<Text>Nothing to show</Text>);
+    const { colors } = useTheme();
+    const { current } = useCardAnimation();
 
-  const { colors } = useTheme();
-  const { current } = useCardAnimation();
+    const onButtonToggle = value => {
+        setViewSelection(viewSelection === qrView ? textView : qrView);
+    };
+
+    useEffect(() => {
+        switch(viewSelection) {
+            case qrView:
+                setViewOut(<QRCode
+                    value={ route.params.qrdata }
+                    size={300}
+                    logo={require('../assets/ATALAPRISM.png')}
+                    logoSize={50}
+                    logoBackgroundColor='transparent'
+                    key="{qr}"
+                />)
+                setViewIcon("toggle-switch")
+                break;
+            case textView:
+                setViewOut(<Text>{ route.params.qrdata }</Text>)
+                setViewIcon("toggle-switch-off-outline")
+                break;
+            default:
+                console.error("Unknown view",viewSelection)
+                break;
+        }
+    }, [viewSelection]);
 
   return (
     <View
@@ -55,16 +88,13 @@ export default function ShowQRCodeScreen({ route, navigation }) {
           ],
         }}
       >
-      {route.params.qrdata.map(qr => (
-        <QRCode
-            value={ JSON.stringify(qr) }
-            size={300}
-            logo={require('../assets/ATALAPRISM.png')}
-            logoSize={50}
-            logoBackgroundColor='transparent'
-            key="{qr}"
-        />
-        ))}
+      <ToggleButton
+            icon={viewIcon}
+            value="toggle view switch"
+            status={viewSelection}
+            onPress={onButtonToggle}
+          />
+      {viewOut}
       </Animated.View>
     </View>
   );
