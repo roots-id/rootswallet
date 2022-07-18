eacd86cb2916ca2f196e2734b47710e9a6c6baab
2byrds <2byrds@gmail.com>
2022-07-18 14:13:50 -0400

    unconfigured AFJ agent provider added
    Signed-off-by: 2byrds <2byrds@gmail.com>

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,30 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-module.exports = getDefaultConfig(__dirname);
+// eslint-disable-next-line
+const { getDefaultConfig } = require('metro-config')
+
+module.exports = (async () => {
+    const {
+        resolver: { sourceExts, assetExts },
+    } = await getDefaultConfig()
+    return {
+        transformer: {
+            babelTransformerPath: require.resolve('react-native-svg-transformer'),
+            getTransformOptions: async () => ({
+                transform: {
+                    experimentalImportSupport: false,
+                    inlineRequires: true,
+                },
+            }),
+        },
+        resolver: {
+            assetExts: assetExts.filter((ext) => ext !== 'svg'),
+            sourceExts: [...sourceExts, 'svg', 'cjs'],
+        },
+    }
+})()
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,13 +1,29 @@
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import Routes from './Routes';
+import AgentProvider from "@aries-framework/react-hooks";
+import React, {useState} from "react";
+import {Agent} from "@aries-framework/core";
 
 export default function Providers() {
   console.log("navigation - setting up providers")
+  const [agent, setAgent] = useState(undefined);
+
+  // const initializeAgent = async () => {
+  //   await // initialize your agent
+  //       setAgent(yourAgent)
+  // }
+  //
+  // useEffect(() => {
+  //   initializeAgent()
+  // }, [])
+
   return (
-      <PaperProvider theme={theme}>
-          <Routes />
-      </PaperProvider>
+      <AgentProvider agent={agent}>
+        <PaperProvider theme={theme}>
+            <Routes/>
+        </PaperProvider>
+      </AgentProvider>
   );
 }
 
