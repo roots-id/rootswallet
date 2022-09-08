eb6f831eead470ff457dcf835d5350cefe613dd5
2byrds <2byrds@gmail.com>
2022-09-08 08:43:56 -0400

    CreateWalletScreen - Creating wallet failed [Error: Exception in HostFunction: kotlinx.serialization.SerializationException: Class 'WalletDocument' is not registered for polymorphic serialization in the scope of 'Wallet'. Mark the base class as 'sealed' or register the serializer explicitly.] [native code]
    Signed-off-by: 2byrds <2byrds@gmail.com>

--- android/app/src/main/java/com/rootswallet/PrismModule.kt
+++ android/app/src/main/java/com/rootswallet/PrismModule.kt
@@ -48,24 +48,26 @@ class PrismModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaM
     }
 
     @ReactMethod(isBlockingSynchronousMethod = true)
-    fun newDID(walJson: String, didAlias: String): String {
-        var cliWal = Json.decodeFromString<Wallet>(walJson);
-        cliWal.addDid(dlt.newDid(didAlias,0,));
-        return Json.encodeToString(cliWal)
+    fun newDID(didAlias: String,seedStr: String,issuer: Boolean): String {
+        val seed = Json.decodeFromString<ByteArray>(seedStr)
+        var did = dlt.newDid(didAlias,0,seed,issuer);
+        return Json.encodeToString(did)
     }
 
     @ReactMethod(isBlockingSynchronousMethod = true)
     fun newWal(name: String, mnemonic: String, passphrase: String): String {
+        Log.d("PRISM_TAG","Creating new wal $name");
         val cliWal = walSer.createWallet(name,mnemonic,passphrase);
         return Json.encodeToString<Wallet>(cliWal);
     }
 
     @ReactMethod
-    fun publishDid(walId: String, didAlias: String, seed:ByteArray, promise: Promise) {
+    fun publishDid(walId: String, didAlias: String, seedStr: String, promise: Promise) {
         Log.d("PRISM_TAG","Publishing "+didAlias);
         thread(start = true) {
             try {
-                var didUpdate = dlt.publishDid(walStore.findDidByAlias(walId,didAlias).get(),seed);
+                val seed = Json.decodeFromString<ByteArray>(seedStr)
+                val didUpdate = dlt.publishDid(walStore.findDidByAlias(walId,didAlias).get(),seed);
                 Log.d("PRISM_TAG","Published $didAlias from with opId ${didUpdate.operationId}")
                 promise.resolve(didUpdate.operationId)
             } catch (e: Exception) {
@@ -75,16 +77,17 @@ class PrismModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaM
     }
 
     @ReactMethod
-    fun issueCred(issuerDidJson: String, seed: ByteArray, credJson: String, promise: Promise) {
+    fun issueCred(issuerDidJson: String, seedStr: String, credJson: String, promise: Promise) {
         Log.d("PRISM_TAG","Issuing credential for $issuerDidJson");
         thread(start = true) {
             try {
                 //var cliWal = Json.decodeFromString<Wallet>(walJson);
                 val cliCred = Json.decodeFromString<IssuedCredential>(credJson);
                 val issuerDid = Json.decodeFromString<Did>(issuerDidJson);
+                val seed = Json.decodeFromString<ByteArray>(seedStr)
                 val dltUpdate = dlt.issueCredential(issuerDid, seed, cliCred)
                 Log.d("PRISM_TAG","Credential $cliCred.verifiedCredential for did $issuerDidJson from wallet $credJson")
-                promise.resolve(dltUpdate.operationId);
+                promise.resolve(Json.encodeToString(dltUpdate));
             } catch (e: Exception) {
                 promise.reject("Issue Credential Error", e);
             }
@@ -92,15 +95,16 @@ class PrismModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaM
     }
 
     @ReactMethod
-    fun revokeCred(credJson: String, issuerDidJson: String, seed: ByteArray, promise: Promise) {
+    fun revokeCred(credJson: String, issuerDidJson: String, seedStr: String, promise: Promise) {
         Log.d("PRISM_TAG","Revoking credential $credJson from issuerDid $issuerDidJson");
         thread(start = true) {
             try {
-                var issuerCred = Json.decodeFromString<IssuedCredential>(credJson);
-                var issuerDid = Json.decodeFromString<Did>(issuerDidJson)
-                var revUpdate = dlt.revokeCredential(issuerCred,issuerDid,seed)
+                val issuerCred = Json.decodeFromString<IssuedCredential>(credJson);
+                val issuerDid = Json.decodeFromString<Did>(issuerDidJson)
+                val seed = Json.decodeFromString<ByteArray>(seedStr)
+                val dltUpdate = dlt.revokeCredential(issuerCred,issuerDid,seed)
                 Log.d("PRISM_TAG","Credential revoked $credJson for issuer $issuerDidJson")
-                promise.resolve(revUpdate.operationId);
+                promise.resolve(Json.encodeToString(dltUpdate));
             } catch (e: Exception) {
                 promise.reject("Revoke Credential Error", e);
             }
@@ -122,9 +126,9 @@ class PrismModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaM
                 var cliWal = Json.decodeFromString<Wallet>(walJson);
                 var verResult = ""
                 if(imported) {
-                    verResult = Json.encodeToString(verifyImportedCredential(cliWal, credAlias))
+                    verResult = Json.encodeToString(dlt.verifyImportedCredential(cliWal, credAlias))
                 } else {
-                    verResult = Json.encodeToString(verifyIssuedCredential(cliWal, credAlias))
+                    verResult = Json.encodeToString(dlt.verifyIssuedCredential(cliWal, credAlias))
                 }
                 Log.d("PRISM_TAG","Credential "+credAlias+" is " + verResult)
                 promise.resolve(verResult);
