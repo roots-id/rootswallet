2cb7eba2a46259e6bf00cb754aba6f828e1ed130
AlexAndrei98 <alexandrei1998@hotmail.it>
2022-12-17 18:27:58 -0600

    Revert "return verifiable.ink diddoc"
    This reverts commit 2c00f40710baa42bfa8a4d39c93f22d20afc12e6.

--- android/app/src/main/java/com/rootswallet/PeerDidModule.kt
+++ android/app/src/main/java/com/rootswallet/PeerDidModule.kt
@@ -86,49 +86,8 @@ class PeerDidModule(reactContext: ReactApplicationContext) : ReactContextBaseJav
     fun resolveDID(did: String, promise: Promise) {
         // request DID Doc in JWK format
         try {
-            var doc = if (did.startsWith("did:peer") == true) {
-            
-            resolvePeerDID(did, format = VerificationMaterialFormatPeerDID.JWK)
-            }
-            else{
-                println("did web to resolve $did")
-            toJson(DIDDoc(
-                        did="did:web:verifiable.ink", 
-                        keyAgreements=listOf("did:web:verifiable.ink#1"), 
-                        authentications=listOf("did:web:verifiable.ink#0"), 
-                        verificationMethods=listOf(
-                            VerificationMethod(
-                                id="did:web:verifiable.ink#0", 
-                                type=VerificationMethodType.JSON_WEB_KEY_2020, 
-                                controller="did:web:verifiable.ink", 
-                                verificationMaterial=VerificationMaterial(
-                                    format=VerificationMaterialFormat.JWK, 
-                                    value=toJson("{'kty': 'OKP', 'crv': 'Ed25519', 'x': '_8eE3ndcEzkNMchUiAaq0NpD4HKC3tKzzbwEmnu0o5o', 'kid': '3587c0177e124eb1b652fa02ac7b3fc1'}")
-                                )
-                            ), 
-                            VerificationMethod(
-                                id="did:web:verifiable.ink#1", 
-                                type=VerificationMethodType.JSON_WEB_KEY_2020, 
-                                controller="did:web:verifiable.ink", 
-                                verificationMaterial=VerificationMaterial(
-                                    format=VerificationMaterialFormat.JWK, 
-                                    value=toJson("{'kty': 'OKP', 'crv': 'X25519', 'x': 'vT4QDzE-s4vTAi9tcouS-3EX70hf-85-yeIg_yEtjV0', 'kid': '13a70f1665a3430596c758e696a93369'}")
-                                )
-                            )
-                        ), 
-                        didCommServices=listOf(
-                            DIDCommService(
-                                id="did:web:verifiable.ink#didcomm", 
-                                serviceEndpoint="https://verifiable.ink/didcomm", 
-                                routingKeys=emptyList(), 
-                                accept=listOf("didcomm/v2"))
-                        )
-                    )
-            )
-            
-            }
-            println("resolved document is $doc")
-            promise.resolve(doc)
+            val didDocJson = resolvePeerDID(did, format = VerificationMaterialFormatPeerDID.JWK)
+            promise.resolve(didDocJson)
         } catch (e: Throwable) {
             promise.reject("Error", e)
         }
